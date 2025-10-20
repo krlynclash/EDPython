c37602ba48b1b6dc43d5244989bcb37a2f6419c3
nike867 <qixinlin43@gmail.com>
2025-10-20 19:39:17 -0400

    test

--- MineCraft.py
+++ MineCraft.py
@@ -1,2 +1,31 @@
-print("10/20/2025!!!!!!afssfakfaf")
-print("Hello World!gsadssgsgsgsg")
+#Cheat Chear
+"""
+1 wood = 4 planks
+1 plank = 2 sticks
+4 planks = 1 crafting table
+
+1 stick + 2 plank = 1 sword
+2 sticks + 1 plank = 1 shovel
+2 sticks + 2 planks = 1 hoe
+2 sticks + 3 planks = axe
+"""
+
+def startGame():
+    #start
+    print("##################")
+    print("Game Started")
+    print("##################")
+
+    continueGame = "y"
+
+    #main loop
+    while (continueGame == "y"):
+        print("Game continued")
+        continueGame = input("Continue another game? (y/n) : ")
+    
+    # end game
+    print("##################")
+    print("Game Ended")
+    print("##################")
+
+startGame()
