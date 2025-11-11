b061b394aec88fa007ceb15902632fa83a3ef2b4
nike867 <qixinlin43@gmail.com>
2025-11-10 20:04:36 -0500

    school

---
+++ School/school.py
@@ -0,0 +1,29 @@
+#global data
+data= []
+filename = "./School/Schools.csv"
+
+def ReadFile():
+    with open(filename, 'r') as file:
+        for line in file:
+            data.append(line.strip().split(','))
+
+
+ReadFile()
+
+print(data)
+
+linecount = 0
+for line in data:
+    linecount += 1
+
+    #print(linecount)
+
+yourschool = "JOSEPH GREENBERG SCHOOL"
+
+
+for line in data:
+    words = line.split(',')
+    for word in words:
+        if word == yourschool:
+            print(word)
+            print(words)
